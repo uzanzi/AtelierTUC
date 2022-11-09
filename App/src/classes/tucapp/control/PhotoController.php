f7bccb7e0fdf844f3440fce048a1e51123124829
ugo zanzi <ugo.zanzi@gmail.com>
2022-11-09 11:22:19 +0100

    Mise en place du controller PhotoController.php

---
+++ App/src/classes/tucapp/control/PhotoController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace iutnc\tweeterapp\control;
+
+
+use iutnc\mf\control\AbstractController;
+
+class PhotoController extends AbstractController
+{
+
+  public function execute(): void
+  {
+
+  }
+}
