5bac1a4f2391021d6ca046ca0fc7f3a5e73fc91a
ugo zanzi <ugo.zanzi@gmail.com>
2022-11-09 11:59:49 +0100

    update PhotoController.php

--- App/src/classes/tucapp/control/PhotoController.php
+++ App/src/classes/tucapp/control/PhotoController.php
@@ -5,12 +5,21 @@ namespace iutnc\tweeterapp\control;
 
 
 use iutnc\mf\control\AbstractController;
+use iutnc\tucapp\model\Photos;
+use iutnc\tweeterapp\view\PhotoView;
 
 class PhotoController extends AbstractController
 {
 
   public function execute(): void
   {
-
+    $idPhoto = $_GET['id'];
+    if ($idPhoto !== 0){
+      $photo = Photos::select()->where("id", "=", $idPhoto)->first();
+      $photoView = new PhotoView($photo);
+      $photoView->setAppTitle($photo->titre);
+      $photoView->addStyleSheet('html/style.css');
+      $photoView->makePage();
+    }
   }
 }
