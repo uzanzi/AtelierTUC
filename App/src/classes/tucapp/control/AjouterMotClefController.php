d44ce380f162cd0c1415d5271bf56c1d7f0cb8c6
lecyp12 <contact@cyprien-cotinaut.com>
2022-11-14 14:23:43 +0100

    création fichier

---
+++ App/src/classes/tucapp/control/AjouterMotClefController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace iutnc\tucapp\control;
+
+use iutnc\mf\control\AbstractController;
+
+class AjouterMotClef extends AbstractController
+{
+
+  public function execute(): void
+  {
+
+  }
+
+}
---
+++ App/src/classes/tucapp/view/AJouterMotClefView.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace iutnc\tucapp\view;
+
+
+class AjouterMotClefView extends TucView
+{
+  public function render(): string {
+    
+    $html = '';
+    return $html;
+  }
+}
